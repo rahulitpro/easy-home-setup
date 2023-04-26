0dae67933bb20c04b13681c89e649ef6b11e169e
Rahul Thakar <rahul@aivism.com>
2023-04-26 16:09:51 +0000

    initial

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -4,3 +4,10 @@ echo "#########################"
 echo "# Updating repositories #"
 echo "#########################"
 sudo apt update
+
+echo "#####################"
+echo "# Installing Docker #"
+echo "#####################"
+sudo apt install docker.io docker-compose -y
+
+
