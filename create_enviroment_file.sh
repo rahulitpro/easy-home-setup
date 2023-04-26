83f1f90c6b84022936ffea47ecacb675994c8a57
Rahul Thakar <rahul@aivism.com>
2023-04-26 22:48:29 +0000

    initial

--- create_enviroment_file.sh
+++ create_enviroment_file.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+if [ ! -f $PWD/.env ]
+then
+	touch .env
+fi
+
+
+check_update_variable(){
+	found=`cat $PWD/.env | grep -v '^$' | grep -v '^#' | grep -w "${2}" | wc -l` > /dev/null 2>&1
+	if [ $found -gt 0 ]
+	then
+		echo "Variable ${2}, already defined"
+		cat $PWD/.env | grep -v '^$' | grep -v '^#' | grep -w "${2}" 
+	else
+		read -p "${1}" VALUE
+		echo "${2}=${VALUE}" >> $PWD/.env
+		cat $PWD/.env | grep -v '^$' | grep -v '^#' | grep -w "${2}"
+	fi
+}
+
+check_update_variable "Enter your email address for let's encrypt (like me@example.com) :" LETSENCRYPT_EMAIL
+check_update_variable "Enter your main domain name with tld (like example.com) :" DOMAIN
+check_update_variable "Enter your traefik subdomain name for traefik dashboard (like traefik) :" TRAEFIK_SUBDOMAIN
+check_update_variable "Enter your Public CIDR for home network to restrict some of the services from public (like 12.34.56.78/32) :" HOME_CIDR 
