6f823cfe3629b9653cf35b9dde0626da3c09b70f
Rahul Thakar <rahul@aivism.com>
2023-04-29 02:33:15 +0000

    auto

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -20,3 +20,10 @@ echo "############################"
 echo "# Installing apache2-utils #"
 echo "############################"
 sudo apt-get install apache2-utils -y
+
+echo "############################"
+echo "# Stopping System resolved #"
+echo "############################"
+sudo systemctl stop systemd-resolved
+sudo systemctl disable systemd-resolved
+sudo sed -i 's/nameserver 127.0.0.53/nameserver 8.8.8.8/g' /etc/resolv.conf
